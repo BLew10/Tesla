9f3b2b3b4f4f5bb9fa71bb3d79e96e4a2d6df9c4
BLew10 <Brandonlewis.10@gmail.com>
2023-01-18 21:37:33 -0800

    existing inventory -> more details

--- client/src/components/SingleCarHome.jsx
+++ client/src/components/SingleCarHome.jsx
@@ -16,7 +16,7 @@ const SingleCarHome = (props) => {
                 <div className='flex flex-col items-center justify-center w-full lg:w-1/2'>
                     <div className={`flex items-center w-full justify-center  ${idx === 3 ? "my-10" : ""}`}>
                         <button className={`rounded-md bg-black bg-opacity-50 font-semibold text-white p-3 w-[250px] mx-5 animate-pulse hover:bg-opacity-75 hover:animate-none hover:scale-105 `} onClick={()=>navigate(`/${car.url}/add`)}>Custom Order</button>
-                        <button className='rounded-md bg-white bg-opacity-50 font-semibold text-black p-3 w-[250px]  mx-5 animate-pulse hover:bg-opacity-75 hover:animate-none hover:scale-105'>Existing Inventory</button>
+                        <button className='rounded-md bg-white bg-opacity-50 font-semibold text-black p-3 w-[250px]  mx-5 animate-pulse hover:bg-opacity-75 hover:animate-none hover:scale-105' onClick={()=>navigate(`/${car.url}`)}>More Details</button>
                     </div>
                     {idx !== 3 ?
                         <img src="https://img.icons8.com/sf-black/64/393c41/expand-arrow.png" className="text-center mb-5 mt-10 animate-bounce cursor-pointer w-[25px]" onClick={() =>
